0debd5053eec27082c99b6e7824c564881fc588e
Kacpermisz <kacpermisz0@gmail.com>
2024-05-22 11:18:31 +0200

    Dodanie wstępnego wyboru

--- menu.py
+++ menu.py
@@ -3,5 +3,8 @@ def menu():
     print("2. Dodaj pacjenta")
     print("3. Podejmij leczenie")
     print("4. Edytuj dane pacjenta")
+    x = input("Witamy w przychodni weterynaryjnej Zwierzogród! Wybierz opcję: ")
+    return x
 
 
+menu()
