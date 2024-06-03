0ce7993b090275210a806be57091f414111409cf
Iga Borowska <iga.borowska@gmail.com>
2024-06-03 19:19:18 +0200

    Fix: poprawiona data + aktualizacja wagi + przeliczanie daty na wiek

--- pacjenci.py
+++ pacjenci.py
@@ -1,4 +1,5 @@
 import json
+from datetime import datetime
 
 def odczyt():
     with open('baza.json', 'r', encoding='utf-8') as file:
@@ -14,7 +15,8 @@ def odczyt_danych_zwierzak(zwierze):
     print(f"Typ: {zwierze['typ']}")
     print(f"Imię: {zwierze['imie']}")
     print(f"Waga: {zwierze['waga']} kg")
-    print(f"Wiek: {zwierze['wiek']} lat")
+    wiek = oblicz_wiek(zwierze['data_urodzenia'])
+    print(f"Wiek: {wiek} lat")
     print(f"Zgon: {'Tak' if zwierze['zgon'] else 'Nie'}")
     print(f"Id chipu: {zwierze['id_chip'] if zwierze['chip'] == True else 'Brak chipu'}")
     print(f"Właściciel: {zwierze['wlasciciel']['imie_nazwisko']}")
@@ -29,6 +31,30 @@ def wyswietl_informacje_zwierzaka():
             print("--------------------")
             print("Dane zwierzaka:")
             odczyt_danych_zwierzak(zwierze)
+#nowa funkcja - powinna  zmieniać datę urodzenia na obecny wiek - działa
+def oblicz_wiek(data_urodzenia):
+    dzisiaj = datetime.now()
+    data_urodzenia = datetime.strptime(data_urodzenia, "%d-%m-%Y")
+    wiek = dzisiaj.year - data_urodzenia.year - ((dzisiaj.month, dzisiaj.day) < (data_urodzenia.month, data_urodzenia.day))
+    return wiek
+
+def wyswietl_informacje_zwierzaka():
+    data = odczyt()
+
+    id_zwierzaka = wybranie_pacjenta()
+    for zwierze in data['zwierzeta']:
+        if zwierze['id'] == id_zwierzaka:
+            wiek = oblicz_wiek(zwierze['data_urodzenia'])
+            print("--------------------")
+            print("Dane zwierzaka:")
+            print(f"ID: {zwierze['id']}")
+            print(f"Typ: {zwierze['typ']}")
+            print(f"Imię: {zwierze['imie']}")
+            print(f"Waga: {zwierze['waga']} kg")
+            print(f"Wiek: {wiek} lat/a")
+            print(f"Zgon: {'Tak' if zwierze['zgon'] else 'Nie'}")
+            print(f"Właściciel: {zwierze['wlasciciel']['imie_nazwisko']}")
+            print(f"Numer telefonu właściciela: {zwierze['wlasciciel']['telefon']}")
             print("Historia leczenia:")
             for zabieg in zwierze['historia_leczenia']:
                 print(f"  - ID zabiegu: {zabieg['id_zabiegu']}")
